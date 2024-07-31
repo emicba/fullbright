b47c324aa64d93a4d24664c66c118c742935d1d5
Emiliano Córdoba <31087818+emicba@users.noreply.github.com>
2024-07-30 22:50:07 -0300

    add actual full bright keybind lol

--- src/client/java/com/emicba/Fullbright.java
+++ src/client/java/com/emicba/Fullbright.java
@@ -1,9 +1,29 @@
 package com.emicba;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class Fullbright implements ClientModInitializer {
+	private static final Double FULLBRIGHT_GAMMA = 16.0;
+
 	@Override
 	public void onInitializeClient() {
+		var keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.fullbright.toggleFullbright",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_N,
+				"category.fullbright.name"
+		));
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (keyBinding.wasPressed()) {
+				var gamma = MinecraftClient.getInstance().options.getGamma();
+				gamma.setValue(gamma.getValue().equals(FULLBRIGHT_GAMMA) ? 1.0 : FULLBRIGHT_GAMMA);
+			}
+		});
 	}
 }
---
+++ src/client/java/com/emicba/mixin/MixinSimpleOption.java
@@ -0,0 +1,30 @@
+package com.emicba.mixin;
+
+import net.minecraft.client.option.SimpleOption;
+import net.minecraft.client.resource.language.I18n;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SimpleOption.class)
+public class MixinSimpleOption<T> {
+    @Shadow
+    @Final
+    Text text;
+
+    @Shadow
+    T value;
+
+    @Inject(method = "setValue", at = @At("HEAD"), cancellable = true)
+    private void setRealValue(T value, CallbackInfo info) {
+        if (text.getString().equals(I18n.translate("options.gamma"))) {
+            this.value = value;
+            // Cancel the "Illegal option value" warning
+            info.cancel();
+        }
+    }
+}
