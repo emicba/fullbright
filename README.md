c5e681af3f31cc62dc12cc8229c307710aabf5d4
Emiliano Córdoba <31087818+emicba@users.noreply.github.com>
2024-07-30 22:10:48 -0300

    setup mod

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Fabric Example Mod
+# Fullbright Mod
 
 ## Setup
 
---
+++ src/client/java/com/emicba/Fullbright.java
@@ -0,0 +1,9 @@
+package com.emicba;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Fullbright implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
